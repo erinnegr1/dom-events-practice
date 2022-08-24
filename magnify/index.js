6f73e5e58710a2e6aeef6b3a40b5cc826498c882
Joseph Dabit <josephdabit@Josephs-MacBook-Air.local>
2022-08-24 15:01:41 -0600

    exersize 2

--- magnify/index.js
+++ magnify/index.js
@@ -5,25 +5,29 @@
 
 let btnMinimize=document.querySelector("#btnMin") // grab the id for button
 let size= 20;  // assigned a new variable text to a number
+const lowNumber = 1;
 btnMinimize.addEventListener('click', function() {  //added event listener to listen for click of butten
  size--  // decrementing by 1 each time it's clicked
  document.querySelector("#textstyle").style.fontSize = size+"px" //took the id textstyel and assigned it the fontsize to our variable text
 // if statement to check if the px number is >< 1px.
 // if greater, continue,
 // else stop
+if (size <= 1) {
+  document.querySelector("#textstyle").style.fontSize = lowNumber+"px" 
+}
 })
- 
- 
- 
- // for (let i = 20; i< 101; i++)  // at each click loop through and increase the number
- /*let variable =document.querySelector("#textstyle") // grab the element that holds the style font size
- textstyle.textContent=text;  // set the number from the for loop to equal the font size of the element
-  
-  
-  console.log(i);
-  let =document.querySelector("#display")
-  counter.textContent=display;
 
-  //textsize = textelement size
+
+let btnMaximize=document.querySelector("#btnMax") // grab the id for button
+let size2 = 20;  // assigned a new variable text to a number
+const highNumber =100;
+btnMaximize.addEventListener('click', function() {  //added event listener to listen for click of butten
+ size2++  // decrementing by 1 each time it's clicked
+ document.querySelector("#textstyle").style.fontSize = size2+"px" //took the id textstyel and assigned it the fontsize to our variable text
+// if statement to check if the px number is >< 1px.
+// if lower, continue,
+// else stop
+if (size2>=100) {
+  document.querySelector("#textstyle").style.fontSize = highNumber+"px" 
+}
 })
-*/
